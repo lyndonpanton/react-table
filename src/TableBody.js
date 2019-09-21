ff0218f1f72061d92feda6d552f2b1f67ecaac7b
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-21 19:55:34 +0100

    created, exported component

---
+++ src/TableBody.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class TableBody extends React.Component {
+	render() {
+		return (
+			<tbody>
+				<tr>
+					<td>Alice</td>
+					<td>20</td>
+					<td>Architect</td>
+				</tr>
+				<tr>
+					<td>Bob</td>
+					<td>21</td>
+					<td>Builder</td>
+				</tr>
+				<tr>
+					<td>Charlotte</td>
+					<td>22</td>
+					<td>Carpenter</td>
+				</tr>
+				<tr>
+					<td>Daniel</td>
+					<td>23</td>
+					<td>Dancer</td>
+				</tr>
+				<tr>
+					<td>Ellouise</td>
+					<td>24</td>
+					<td>Expeditionist</td>
+				</tr>
+				<tr>
+					<td>Freddie</td>
+					<td>25</td>
+					<td>Florist</td>
+				</tr>
+				<tr>
+					<td>Gabriella</td>
+					<td>26</td>
+					<td>Gamer</td>
+				</tr>
+				<tr>
+					<td>Harry</td>
+					<td>27</td>
+					<td>Hair Stylist</td>
+				</tr>
+			</tbody>
+		);
+	}
+}
+
+export default TableBody;
