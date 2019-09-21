7402e8f09018c854ad8a319eac476ac0b99220c9
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-21 19:51:59 +0100

    created app component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,13 @@
 import React from "react";
 import Table from "./Table";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<h1>Hello React</h1>
+				<Table />
+			</div>
+		);
+	}
+}
