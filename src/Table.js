76310a959aee703ff2d09d20c1dccf29e54cf0ea
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-21 19:48:16 +0100

    created table component fron table components

--- src/Table.js
+++ src/Table.js
@@ -2,3 +2,15 @@ import React from "react";
 import TableHead from "./TableHead";
 import TableBody from "./TableBody";
 import TableFoot from "./TableFoot";
+
+class Table extends React.Component {
+	render() {
+		return (
+			<table>
+				<TableHead />
+				<TableBody />
+				<TableFoot />
+			</table>
+		);
+	}
+}
