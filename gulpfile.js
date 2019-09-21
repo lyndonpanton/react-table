fc749031fe530570ad9e83cfa8315f261c7eab96
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-21 20:14:22 +0100

    created task to compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,10 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+
+// compile sass
+gulp.task("sass", function() {
+	gulp.src("src/*.sass")
+		.pipe(sass())
+		.on("error", sass.logError)
+		.pipe(gulp.dest("src"));
+});
