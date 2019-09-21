a98a21e3966af2bbe7f57c87a4173a5789d7770d
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-21 19:41:40 +0100

    created and exported component

---
+++ src/TableFoot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class TableFoot extends React.Component {
+	render() {
+		return (
+			<tfoot>
+				<tr>
+					<td colspan="3">This table is rendered using ReactJS</td>
+				</tr>
+			</tfoot>
+		);
+	}
+}
+
+export default TableFoot;
