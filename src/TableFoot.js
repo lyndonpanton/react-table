9f41a53d7f03db783c92836217a5cdb2ab436d13
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 20:09:12 +0100

    changed stateful component to stateless component

--- src/TableFoot.js
+++ src/TableFoot.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class TableFoot extends React.Component {
-	render() {
-		return (
-			<tfoot>
-				<tr>
-					<td colSpan="4">This table is rendered using ReactJS</td>
-				</tr>
-			</tfoot>
-		);
-	}
+const TableFoot = () => {
+	return (
+		<tfoot>
+			<tr>
+				<td colspan="4">This table is rendered using ReactJS</td>
+			</tr>
+		</tfoot>
+	);
 }
 
 export default TableFoot;
