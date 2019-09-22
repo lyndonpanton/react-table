25fab40c2dfa44b9b8d37ea7fa82ff35953fd803
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 20:11:09 +0100

    changed stateful component to stateless component

--- src/TableHead.js
+++ src/TableHead.js
@@ -1,18 +1,14 @@
-import React from "react";
-
-class TableHead extends React.Component {
-	render() {
-		return (
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Age</th>
-					<th>Job</th>
-					<th>Remove</th>
-				</tr>
-			</thead>
-		);
-	}
+const TableHead = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Age</th>
+				<th>Job</th>
+				<th>Remove</th>
+			</tr>
+		</thead>
+	);
 }
 
 export default TableHead;
