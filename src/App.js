a55ba10ff09280b3f58fe10050be9c5b28cb42c4
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 18:34:06 +0100

    added people data to state object

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import React from "react";
 import Table from "./Table";
 
 class App extends React.Component {
-	state = {};
-	
+	state = {
+		peopleData: [
+			{
+				"name": "Alice",
+				"age": 20,
+				"job": "Architect"
+			},
+			{
+				"name": "Bob",
+				"age": 21,
+				"job": "Builder"
+			},
+			{
+				"name": "Charlotte",
+				"age": 22,
+				"job": "Carpenter"
+			},
+			{
+				"name": "Daniel",
+				"age": 23,
+				"job": "Dancer"
+			},
+			{
+				"name": "Ellouise",
+				"age": 24,
+				"job": "Expeditionist"
+			},
+			{
+				"name": "Freddie",
+				"age": 25,
+				"job": "Florist"
+			},
+			{
+				"name": "Gabriella",
+				"age": 26,
+				"job": "Gamer"
+			},
+			{
+				"name": "Harry",
+				"age": 27,
+				"job": "Hair Stylist"
+			}
+		]
+	};
+
 	render() {
 		const peopleData = [
 			{
