76303f01391e9f37fb35148f9a4231e30fbd5b8d
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 18:48:05 +0100

    table data is now taken from state
    instead of array defined in render method

--- src/App.js
+++ src/App.js
@@ -62,48 +62,7 @@ class App extends React.Component {
 	};
 
 	render() {
-		const peopleData = [
-			{
-				"name": "Alice",
-				"age": 20,
-				"job": "Architect"
-			},
-			{
-				"name": "Bob",
-				"age": 21,
-				"job": "Builder"
-			},
-			{
-				"name": "Charlotte",
-				"age": 22,
-				"job": "Carpenter"
-			},
-			{
-				"name": "Daniel",
-				"age": 23,
-				"job": "Dancer"
-			},
-			{
-				"name": "Ellouise",
-				"age": 24,
-				"job": "Expeditionist"
-			},
-			{
-				"name": "Freddie",
-				"age": 25,
-				"job": "Florist"
-			},
-			{
-				"name": "Gabriella",
-				"age": 26,
-				"job": "Gamer"
-			},
-			{
-				"name": "Harry",
-				"age": 27,
-				"job": "Hair Stylist"
-			}
-		];
+		const peopleData = this.state.peopleData;
 
 		return (
 			<div className="container">
