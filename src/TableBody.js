db163eae39bbaf3f71bcf2bbfb70a51d44ed3966
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 20:18:29 +0100

    changed stateful component to stateless component

--- src/TableBody.js
+++ src/TableBody.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-class TableBody extends React.Component {
-	render() {
-		const rows = this.props.peopleData.map(function(row, index) {
-			return (
-				<tr key={index}>
-					<td>{row.name}</td>
-					<td>{row.age}</td>
-					<td>{row.job}</td>
-					<td>
-						<button>Delete</button>
-					</td>
-				</tr>
-			);
-		});
-
+const TableBody = (props) => {
+	const rows = props.peopleData.map(function(row, index) {
 		return (
-			<tbody>{rows}</tbody>
+			<tr key={index}>
+				<td>{row.name}</td>
+				<td>{row.age}</td>
+				<td>{row.job}</td>
+				<td>
+					<button>Delete</button>
+				</td>
+			</tr>
 		);
-	}
+	});
+
+	return (
+		<tbody>{rows}</tbody>
+	);
 }
 
 export default TableBody;
