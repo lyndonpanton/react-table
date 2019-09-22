40d5aacf82cb35587a8d00165503f69855f5719d
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-22 16:45:21 +0100

    added array with table data

--- src/TableBody.js
+++ src/TableBody.js
@@ -2,6 +2,48 @@ import React from "react";
 
 class TableBody extends React.Component {
 	render() {
+		const people = [
+			{
+				name: "Alice",
+				age: 20,
+				job: "Architect"
+			},
+			{
+				name: "Bob",
+				age: 21,
+				job: "Builder"
+			},
+			{
+				name: "Charlotte",
+				age: 22,
+				job: "Carpenter"
+			},
+			{
+				name: "Daniel",
+				age: 23,
+				job: "Dancer"
+			},
+			{
+				name: "Ellouise",
+				age: 24,
+				job: "Expeditionist"
+			},
+			{
+				name: "Freddie",
+				age: 25,
+				job: "Florist"
+			},
+			{
+				name: "Gabriella",
+				age: 26,
+				job: "Gamer"
+			},
+			{
+				name: "Harry",
+				age: 27,
+				job: "Hair Stylist"
+			}
+		];
 		return (
 			<tbody>
 				<tr>
